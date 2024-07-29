b0a81d4f6e58cf386703242e6cbdf834acaf9af8
kayumov1554 <167546948+kayumov1554@users.noreply.github.com>
2024-07-29 21:31:37 +0500

    sa

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from flask import Flask, jsonify
 import asyncio
 from contextlib import suppress
-
 from bot.utils.launcher import process
 
+app = Flask(__name__)
+
+@app.route('/start', methods=['GET'])
+def start_process():
+    with suppress(KeyboardInterrupt):
+        asyncio.run(main())
+    return jsonify({"status": "process started"})
 
 async def main():
     await process()
 
-
 if __name__ == '__main__':
-    with suppress(KeyboardInterrupt):
-        asyncio.run(main())
+    app.run(host='0.0.0.0', port=5000)
